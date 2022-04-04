3f29a527d200a98d297130b5aef78a8f074d2235
Yuta Nakamura <yutanak2010@gmail.com>
2022-04-04 13:00:41 -0700

    Test: Added tests for checking to see if the code is left aligned, and for the if/else being in the same line. Readme: Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# style-scoring
+# Style Scorer
+This is a style scorer that scores programs according to a set coding style. The main checker program is just a modified version of cpplint from here[github.com/cpplint/cpplint].
---
+++ tests/test13_IndentLeftAlgin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+int x;
+cin >> x;
+int n = 0;
+for (int i = 0; i < x; i++) {
+n += i;
+if (n < 0) {
+n *= n;
+cout << i << endl;
+cout << n << endl;
+}
+cout << n*n << endl;
+}
+}
